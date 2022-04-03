6b460a1f603e5b9ef77c086f6469691bbaf79798
littae2 <littae2@student.op.ac.nz>
2022-04-03 16:56:07 +1200

    clearing folder

--- Source/SirenGame/SirenGame.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "SirenGame.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SirenGame, "SirenGame" );
